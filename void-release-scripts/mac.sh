8061fa89eda88bdc43069e1de297ad4ceda11281
Andrew Pareles <andrewpareles@gmail.com>
2025-02-12 01:56:31 -0800

    +

--- void-release-scripts/mac.sh
+++ void-release-scripts/mac.sh
@@ -10,7 +10,6 @@ set -e
 ./mac-sign.sh notarize arm64
 ./mac-sign.sh updater arm64
 ./mac-sign.sh hash arm64
-
 ./mac-sign.sh buildreh arm64
 ./mac-sign.sh packagereh arm64
 
@@ -20,6 +19,5 @@ set -e
 ./mac-sign.sh notarize x64
 ./mac-sign.sh updater x64
 ./mac-sign.sh hash x64
-
 ./mac-sign.sh buildreh x64
 ./mac-sign.sh packagereh x64
