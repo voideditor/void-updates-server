029ac4a248edf7d38f4e0be7bfeae08bf8be97b0
Andrew Pareles <andrewpareles@gmail.com>
2025-03-22 18:31:55 -0700

    +

--- void-release-scripts/mac.sh
+++ void-release-scripts/mac.sh
@@ -11,21 +11,21 @@ set -e
 
 # Build, sign and package arm64
 ./mac-sign.sh build arm64
-./mac-sign.sh buildreh arm64
 ./mac-sign.sh sign arm64
 ./mac-sign.sh notarize arm64
 ./mac-sign.sh rawapp arm64
 ./mac-sign.sh hashrawapp arm64
-./mac-sign.sh packagereh arm64
+
+# ./mac-sign.sh buildreh arm64
+# ./mac-sign.sh packagereh arm64
 
 # Build, sign and package x64
 ./mac-sign.sh build x64
-./mac-sign.sh buildreh x64
 ./mac-sign.sh sign x64
 ./mac-sign.sh notarize x64
 ./mac-sign.sh rawapp x64
 ./mac-sign.sh hashrawapp x64
-./mac-sign.sh packagereh x64
 
+# ./mac-sign.sh buildreh x64
+# ./mac-sign.sh packagereh x64
 
-# TODO: 1. make sure .zip is signed, 2. recursively codesign app
